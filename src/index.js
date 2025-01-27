bb608c502362fbed714b955916283d61011e290a
Plhume <72684309+Plhume@users.noreply.github.com>
2025-01-27 01:05:28 +0100

    Create base of client

--- src/index.js
+++ src/index.js
@@ -1,1 +1,52 @@
-// Refactoring V1 -> V2
+const { Client, GatewayIntentBits, Partials } = require("discord.js");
+const Logger = require('./utils/logger.js');
+const Config = require('./config/config');
+
+class Alyoria {
+    constructor() {
+        this.client = new Client({
+            intents: [
+                GatewayIntentBits.Guilds,
+                GatewayIntentBits.GuildMembers,
+                GatewayIntentBits.GuildMessages,
+                GatewayIntentBits.GuildPresences,
+                GatewayIntentBits.GuildVoiceStates,
+                GatewayIntentBits.GuildInvites,
+                GatewayIntentBits.GuildMessageReactions,
+                GatewayIntentBits.MessageContent
+            ],
+            partials: [
+                Partials.Channel,
+                Partials.Message,
+                Partials.User
+            ],
+            presence: {
+                activities: [{
+                    name: Config.PRESENCE.ACTIVITY[0].NAME,
+                    type: Config.PRESENCE.ACTIVITY[0].TYPE
+                }],
+                status: Config.PRESENCE.STATUS
+            }
+        });
+        this.config = Config;
+        this.logger = Logger;
+    }
+
+    async start() {
+        try {
+            this.logger.clear();
+            this.logger.info(`Connecting to Discord...`);
+
+            await this.client.login(this.config.CLIENT.TOKEN);
+
+            this.logger.info(`${this.client.user.username} is now online!`);
+        } catch (err) {
+            this.logger.error(err);
+        }
+    }
+}
+
+const bot = new Alyoria();
+bot.start();
+
+module.exports = bot;
